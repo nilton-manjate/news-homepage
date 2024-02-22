49474b7677da4c34b0fe4e948d98bf01768c6913
nilton-manjate <ntnmanjate1@gmail.com>
2024-02-22 15:04:26 +0200

    add mobile style

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors: {
+        'softOrange': 'hsl(35, 77%, 62%)',
+        'softRed': 'hsl(5, 85%, 63%)',
+        'offWhite': 'hsl(36, 100%, 99%)',
+        'grayishBlue': 'hsl(233, 8%, 79%)',
+        'darkGrayishBlue': 'hsl(236, 13%, 42%)',
+        'veryDarkBlue': 'hsl(240, 100%, 5%)'
+      },
+
+      backgroundImage: {
+        'mobile': "url('../images/image-web-3-mobile.jpg')",
+        'desktop': "url('../images/image-web-3-desktop.jpg'"
+      },
+
+      fontFamily: {
+        'inter': ['Inter', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
